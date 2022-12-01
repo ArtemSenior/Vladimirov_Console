6c921a7d19ef8eec422dc4183d2536e3d51e0e70
ArtemSenior <119601918+ArtemSenior@users.noreply.github.com>
2022-12-01 16:34:21 +0300

    Add files via upload

---
+++ Console1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p, s;
+            do
+            {
+                try
+                {
+                    Console.Write("p = ");
+                    p = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Вводить только числа!!!");
+                }
+            }
+            while (true);
+            s = Math.Pow(p, 2) * Math.Sqrt(3) / 36;
+            Console.WriteLine("Площадь равностороннего треугольника равна " + s);
+        }
+    }
+}
---
+++ Console1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 0;
+            do
+            {
+                try
+                {
+                    Console.Write("Проверка на четность числа. Введите число: ");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    Console.WriteLine("Вводить только целые числа!!!");
+                }
+            }
+            while (true);
+            if (a % 2 == 0)
+                    {
+                        Console.WriteLine("Число "+a+" является четным.");
+                    }
+                        else
+                        {
+                            Console.WriteLine("Число " + a + " четным НЕ ЯВЛЯЕТСЯ!");
+                        }
+        }
+    }
+}
