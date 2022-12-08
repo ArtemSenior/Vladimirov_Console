3553919ea9e73242ce6cff073abdc62dc0c8b521
ArtemSenior <119601918+ArtemSenior@users.noreply.github.com>
2022-12-09 00:15:32 +0300

    Add files via upload

---
+++ CНеделя2_Задание4_1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static double F(double n)
+        {
+            if (n == 1) return -10;
+            if (n == 2) return 2;
+            return Math.Abs(F(n - 2)) - 6 * F(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+                try
+                {
+                    Console.Write("Введите n: ");
+                    double x = Convert.ToDouble(Console.ReadLine());
+                    if (x <= 0)
+                    {
+                        Console.WriteLine("Нельзя вводить числа меньше 0!");
+                    }
+                    else
+                    {
+                        Console.WriteLine(F(x));
+                        break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Вводить только числа");
+                }
+            }
+            while (true);
+        }
+    }
+}
---
+++ CНеделя2_Задание4_2/CНеделя2_Задание4_2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Rec(double a0, double d, int n, double s, out double an, out double sum)
+        {
+            an = a0;
+            s += a0;
+            sum = s;
+            if (n == 1) return;
+            Rec(a0 + d, d, n - 1, s, out an, out sum);
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+                double a0 = 0;
+                double d = 0;
+                int n = 0;
+                do
+                {
+                    try
+                    {
+                        Console.Write("a0 = ");
+                        a0 = double.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("Вводите только числа");
+                    }
+                }
+                while (true);
+
+                do
+                {
+                    try
+                    {
+                        Console.Write("d = ");
+                        d = double.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Вводите только числа");
+                    }
+                }
+                while (true);
+
+                do
+                {                    
+                    try
+                    {
+                        Console.Write("n = ");
+                        n = int.Parse(Console.ReadLine());
+                        if (n > 0) ;
+                        break;
+                    }
+
+                    catch
+                    {
+
+                        Console.WriteLine("Где n > 1");
+                    }
+
+                }
+
+                while (true);
+
+                double an, sum;
+                Rec(a0, d, n, 0, out an, out sum);
+                Console.WriteLine($"An = {an}, sum = {sum}");
+                break;
+
+            } while (true);
+        }
+    }
+}
