de4d8bf0618d4a465eff46680e91793a16c68ae1
ArtemSenior <119601918+ArtemSenior@users.noreply.github.com>
2022-12-26 08:04:22 +0300

    Add files via upload

---
+++ CНеделя3_Задание10/CНеделя3_Задание10/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CНеделя3_Задание10
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Directory.CreateDirectory("c:\\temp");
+                Directory.CreateDirectory("c:\\temp\\K1");
+                Directory.CreateDirectory("c:\\temp\\K2");
+                StreamWriter sw = new StreamWriter("c:\\temp\\K1\\t1.txt");
+                sw.Write("Владимиров Артем Игоревич, 08.11.2001, Россия г.Владимир");
+                sw.Close();
+                sw = new StreamWriter("c:\\temp\\K1\\t2.txt");
+                sw.Write("Владимиров Артем Игоревич, 08.11.2002, Россия г.Владимир");
+                sw.Close();
+
+                sw = new StreamWriter("c:\\temp\\K2\\t3.txt");
+                StreamReader sr = new StreamReader("c:\\temp\\K1\\t1.txt");
+                var read = sr.ReadToEnd();
+                sw.WriteLine(read);
+                sr.Close();
+
+                sr = new StreamReader("c:\\temp\\K1\\t2.txt");
+                read = sr.ReadToEnd();
+                sw.WriteLine(read);
+                sr.Close();
+                sw.Close();
+
+                DirectoryInfo d = new DirectoryInfo("c:\\temp\\K1\\");
+                DirectoryInfo d3 = new DirectoryInfo("c:\\temp\\K2\\");
+                FileInfo[] f = d.GetFiles();
+                FileInfo[] f3 = d3.GetFiles();
+                Console.WriteLine("K1:");
+                foreach (FileInfo i in f)
+                {
+                    Console.WriteLine("Имя: " + i.Name.ToString() + "\n"
+                        + "Путь: " + i.FullName.ToString() + "\n"
+                        + "Размер: " + i.Length.ToString() + "кб\n"
+                        + "Время создания: " + i.CreationTime.ToString() + "\n");
+                }
+                Console.WriteLine("K2:");
+                foreach (FileInfo i in f3)
+                {
+                    Console.WriteLine("Имя: " + i.Name.ToString() + "\n"
+                        + "Путь: " + i.FullName.ToString() + "\n"
+                        + "Размер: " + i.Length.ToString() + "кб\n"                    
+                        + "Время создания: " + i.CreationTime.ToString() + "\n");
+                }
+
+
+                File.Move("c:\\temp\\K1\\t2.txt", "c:\\temp\\K2\\t2.txt");
+                File.Copy("c:\\temp\\K1\\t1.txt", "c:\\temp\\K2\\t1.txt");
+                Directory.Move("c:\\temp\\K2", "c:\\temp\\ALL");
+                Directory.Delete("c:\\temp\\K1", true);
+
+                DirectoryInfo h = new DirectoryInfo("c:\\temp\\ALL\\");
+                FileInfo[] j = h.GetFiles();
+                Console.WriteLine("All:");
+                foreach (FileInfo i in j)
+                {
+                    Console.WriteLine("Имя: " + i.Name.ToString() + "\n"
+                        + "Путь: " + i.FullName.ToString() + "\n"
+                        + "Размер: " + i.Length.ToString() + "кб\n"
+                        + "Время создания: " + i.CreationTime.ToString() + "\n");
+            }
+            }
+        }
+}
---
+++ CНеделя3_Задание11/CНеделя4_Задание11/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace CНеделя3_Задание11
+{
+    class Program
+    {
+        private static int readInt()
+        {
+            do
+            {   // Проверка на ввод данных
+                int result;
+                bool parsed = int.TryParse(Console.ReadLine(), out result);
+                if (parsed) return result;
+                else Console.Write("Введите корректное число: ");
+            } while (true);
+        }
+        class Point  // Класс Point
+        {
+            public int x;
+            public int y;
+
+            public Point() // Конструктор при нулевых координатах
+            {
+                x = 0;
+                y = 0;
+            }
+            public Point(int X, int Y) // Конструктор при заданных координатах
+            {
+                x = X;
+                y = Y;
+            }
+            public void schowPoints() // Метод1 вывода координат точек
+            {
+                Console.WriteLine($"x: {x}, y: {y}");
+            }
+            public void showCalculate() // Метод2 вывода расстояния от начала координат до точки 
+            {
+                double s;
+                s = Math.Sqrt((x - 0) * (x - 0) + (y - 0) * (y - 0));
+                Console.WriteLine($"Расстояние от начала координат до точки: {s}");
+            }
+            public void Moving(int a, int b) // Метод3 вычесления и вывода координат точки после перемещения на вектор a,b
+            {
+                x = x + a;
+                y = y + b;
+                Console.WriteLine($"Координаты точки после перемещения на вектор (a,b) x: {x}, y: {y}");
+            }
+            public int X // Свойства1 для записи и вывода координат точки
+            {
+                get
+                {
+                    return x;
+                }
+                set
+                {
+                    x = value;
+                }
+
+            }
+            public int Y
+            {
+                get
+                {
+                    return y;
+                }
+                set
+                {
+                    y = value;
+                }
+
+            }
+            public int SkalarX // Свойство2 позволяющее вывести умножить координаты точки на скаляр
+            {
+                set
+                {
+                    x = x * value;
+                }
+            }
+            public int SkalarY
+            {
+                set
+                {
+                    y = y * value;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целочисленные координаты");
+            Console.Write("Введите x: ");
+            int x = readInt();
+            Console.Write("Введите y: ");
+            int y = readInt();
+
+            Point point = new Point(x, y);
+            Point emptyPoint = new Point();
+
+            Console.WriteLine("Координаты точки:");
+            Console.WriteLine("Координаты точки с нулевыми координтами:");
+            emptyPoint.schowPoints();
+            Console.WriteLine("Координаты точки с заданными координатами:");
+            point.schowPoints();
+            Console.WriteLine();
+
+            Console.WriteLine("Tочка с заданными координатами:");
+            point.showCalculate();
+            Console.WriteLine("Tочка с нулевыми координатами:");
+            emptyPoint.showCalculate();
+            Console.WriteLine();
+
+            Console.WriteLine("Перемещение точки на вектор");
+            Console.WriteLine("Введите у вектора a: ");
+            int a = readInt();
+            Console.WriteLine("Введите у вектора b: ");
+            int b = readInt();
+            point.Moving(a, b);
+            emptyPoint.Moving(a, b);
+            Console.WriteLine("Tочка с заданными координатами:");
+            point.schowPoints();
+            Console.WriteLine("Tочка с нулевыми координатами:");
+            emptyPoint.schowPoints();
+            Console.WriteLine();
+
+            Console.WriteLine("Получение и установление координаты точке через свойства");
+            Console.WriteLine("Введите x: ");
+            int X = readInt();
+            Console.WriteLine("Введите y: ");
+            int Y = readInt();
+            point.X = X;
+            point.Y = Y;
+            Console.WriteLine("X: " + point.X);
+            Console.WriteLine("Y: " + point.Y);
+            Console.WriteLine();
+
+            Console.WriteLine("Умножение координаты точки на скаляр");
+            Console.WriteLine("Введите скаляр: ");
+            int SCA = readInt();
+            point.SkalarX = SCA;
+            point.SkalarY = SCA;
+            Console.WriteLine("X: " + point.X);
+            Console.WriteLine("Y: " + point.Y);
+            Console.WriteLine();
+        }
+    }
+}
